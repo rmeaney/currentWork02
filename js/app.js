7e9385010c8c063d5d1749fc5a1be13dc3df4650
Richard Meaney <Richard Meaney>
2017-07-10 22:45:14 -0400

    braking

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ $(function() {
           $.each(results.items, function(index, item) {
             $.get("tpl/item.html", function(data) {
               console.log(data);
-                $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+                $("#results").append(tplawesome(data, [{"title":item.snippet.title}]));
                 //$("#results").append(this.videoId);
             });
           });
