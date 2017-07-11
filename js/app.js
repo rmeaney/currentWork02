e1b538389598831d8913115066dd70dc78dcbcdd
Richard Meaney <Richard Meaney>
2017-07-10 22:31:02 -0400

    sd

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ $(function() {
           $("#results").html("");
           $.each(results.items, function(index, item) {
             $.get("tpl/item.html", function(data) {
-              console.log(results);
+              console.log(data);
                 $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
                 $("#results").append('<p>hello I was added!</p>');
             });
